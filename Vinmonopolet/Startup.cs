8d26fa1ec0291f763d5d63c86c6748d5dc25c1bc
Per-Kristian Helland <per-kristian.helland@komplett.com>
2017-08-17 20:57:30 +0200

    Revert Azure debug.

--- Vinmonopolet/Startup.cs
+++ Vinmonopolet/Startup.cs
@@ -42,15 +42,15 @@ namespace Vinmonopolet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseDatabaseErrorPage();
             if (env.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
                 app.UseBrowserLink();
             }
             else
             {
-                // app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
