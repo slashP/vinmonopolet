d1a7fa9bf84eb9dfc4089f45e70fe445a6e1ea70
Per-Kristian Helland <per-kristian.helland@komplett.com>
2017-08-17 19:42:55 +0200

    Debug Azure failure.

--- Vinmonopolet/Startup.cs
+++ Vinmonopolet/Startup.cs
@@ -42,9 +42,9 @@ namespace Vinmonopolet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
                 app.UseDatabaseErrorPage();
             }
