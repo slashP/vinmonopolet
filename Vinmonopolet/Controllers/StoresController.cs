36064f7102efa682121fb1692a84c35404c5c5e3
Per-Kristian Helland <per-kristian.helland@komplett.com>
2018-04-09 18:49:14 +0200

    It should be possible to toggle stores between active and not when logged in.

--- Vinmonopolet/Controllers/StoresController.cs
+++ Vinmonopolet/Controllers/StoresController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Vinmonopolet.Data;
@@ -19,6 +20,16 @@ namespace Vinmonopolet.Controllers
         {
             var stores = await _db.Stores.ToListAsync();
             return View(stores);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("stores/toggle/{storeId}")]
+        public async Task<int> Toggle(string storeId)
+        {
+            var store = await _db.Stores.FirstAsync(x => x.Id == storeId);
+            store.IsActive = !store.IsActive;
+            return await _db.SaveChangesAsync();
         }
     }
 }
