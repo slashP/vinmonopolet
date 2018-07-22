85a1623a0621b451bb57cd8cc2357256e47f788d
Lars-Petter Bjørkeng <larspetter.bjorkeng@komplett.no>
2018-07-22 23:22:09 +0200

    I need a simple working client to get data from untappd.

--- Vinmonopolet/Controllers/UntappdController.cs
+++ Vinmonopolet/Controllers/UntappdController.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Vinmonopolet.Services;
 
 namespace Vinmonopolet.Controllers
 {
     public class UntappdController : Controller
     {
+        private IUntappdClient _untappdClient;
 
-
+        public UntappdController(IUntappdClient _client)
+        {
+            _untappdClient = _client;
+        }
+        
+        [HttpPost]
+        public async Task<string> UpdateBeer(string id)
+        {
+            var response = await _untappdClient.BeerInfoCompact(id);
+            return response;
+        }
     }
 }
---
+++ Vinmonopolet/Services/IUntappdClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Vinmonopolet.Services
+{
+    public interface IUntappdClient
+    {
+        Task<string> BeerInfoCompact(string id);
+    }
+}
