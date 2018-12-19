efed2b4dd254b4a8758bf328f031a8c7c272b63c
Lars-Petter Bjørkeng <lars-petter.bjorkeng@clave.no>
2018-12-19 11:21:10 +0100

    Trying to fix build and stuff, wrapped in som css and tooltips :D

--- Vinmonopolet/ClientApp/src/Components/BeerList/StoreStock.jsx
+++ Vinmonopolet/ClientApp/src/Components/BeerList/StoreStock.jsx
@@ -3,22 +3,25 @@ import styles from './styles.css';
 
 
 const StoreStock = ({ stocks, activeStores }) => {
-    stocks.sort((a, b) => b.stockLevel - a.stockLevel )
+    stocks.sort((a, b) => b.stockLevel - a.stockLevel)
+    let showStocks = stocks.length > 4 ? stocks.slice(0, 2) : stocks;
+    let tooltipTitle = stocks.map(stock => "" + stock.storeName + " : " + stock.stockLevel + " \n").join("");
+
     if (!activeStores.length) {
         return (
-            <div style={styles.storeStock}>
-                    <ul style={styles.stockList}>
-                        {stocks.map(stock =>
-                            <li key={stock.storeName}>{stock.storeName} : {stock.stockLevel}</li>
-                        )}
-                    </ul>
+            <div title={tooltipTitle} style={styles.storeStock}>
+                <ul style={styles.stockList}>
+                    {showStocks.map(stock =>
+                        <li key={stock.storeName}>{stock.storeName} : {stock.stockLevel}</li>
+                    )}
+                </ul>
             </div>
         )
     } else {
         return (
-            <div style={styles.storeStock}>
+            <div title={tooltipTitle} style={styles.storeStock}>
                 <ul style={styles.stockList}>
-                    {stocks.map(stock =>
+                    {showStocks.map(stock =>
                         activeStores.includes(stock.storeId) && <li key={stock.storeName}>{stock.storeName} : {stock.stockLevel}</li>
                     )}
                 </ul>
