e7063f6503e37bc978235ac1c131e8f7844f7b72
Lars-Petter Bjørkeng <elpea2k@gmail.com>
2020-02-02 21:19:52 +0100

    Checking if I could just swap out the frontend like this.

---
+++ Vinmonopolet/wwwroot/build/precache-manifest.5eafffb52021146fed6774aa66dd7685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947c440eb4fc87974e2fc9e0aa94a82f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad21d61f9d8f43907e3",
+    "url": "/static/css/main.0d6e3869.chunk.css"
+  },
+  {
+    "revision": "68833f22b52442e41c7b",
+    "url": "/static/js/2.f276145a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.f276145a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ad21d61f9d8f43907e3",
+    "url": "/static/js/main.d84b51ec.chunk.js"
+  },
+  {
+    "revision": "de849b621e9b36141536",
+    "url": "/static/js/runtime-main.292b53d7.js"
+  },
+  {
+    "revision": "420c050500a412f6c63a45653b311f25",
+    "url": "/static/media/filter-solid.420c0505.svg"
+  }
+]);
