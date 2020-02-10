afb3c9b93730528ef3ecf5421a3b29a8d5104264
Lars-Petter Bjørkeng <elpea2k@gmail.com>
2020-02-10 22:35:23 +0100

    Ok. I might have been to agressive here... Lets help it along a bit, even though i dont know whats wrong.

---
+++ Vinmonopolet/wwwroot/precache-manifest.82a3a426df673e69485bc61d65a03775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61105f930ba65db83b1d19a0c6be64ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b180cd9e8f1e6325bc",
+    "url": "/static/css/main.7ccd5a9a.chunk.css"
+  },
+  {
+    "revision": "128e41965fd264413de7",
+    "url": "/static/js/2.37237b4d.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.37237b4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0b180cd9e8f1e6325bc",
+    "url": "/static/js/main.9b152950.chunk.js"
+  },
+  {
+    "revision": "de849b621e9b36141536",
+    "url": "/static/js/runtime-main.292b53d7.js"
+  }
+]);
