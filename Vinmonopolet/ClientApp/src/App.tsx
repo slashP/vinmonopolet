25e9ad1f94c2bfb7b1035fe0655c1151a2ef55d1
Lars-Petter Bjørkeng <lars-petter.bjorkeng@clave.no>
2020-02-17 08:18:53 +0100

    Was this supposed to build on its own now? If so, heres some analytics initialization.

--- Vinmonopolet/ClientApp/src/App.tsx
+++ Vinmonopolet/ClientApp/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 
 import BaseView from './Components/BaseView';
 import Header from './Components/Header/Header';
@@ -7,7 +8,11 @@ import { RawDataContextProvider } from './Contexts/RawDataContext';
 import { FilterContextProvider } from './Contexts/FilterContext';
 import { SortingContextProvider } from './Contexts/SortingContext';
 
+const trackingId = "UA-136322230-2";
+
 const App: React.FC = () => {
+  ReactGA.initialize(trackingId);
+
   return (
     <div className="App">
       <RawDataContextProvider>
