5e963acdbab6a950c50eccdf41df7bd5a5f85a4e
Lars-Petter Bjørkeng <lars-petter.bjorkeng@clave.no>
2020-02-18 14:37:21 +0100

    What about now?

---
+++ Vinmonopolet/wwwroot/build/precache-manifest.287d108f755f8e3cc041652f7d9f5c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19dcdc7d8f51a3bb0cbe9bb4ecf048f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c891bac0072d36b544e2",
+    "url": "/static/css/main.0bd8508c.chunk.css"
+  },
+  {
+    "revision": "e84f9867d366945eb50a",
+    "url": "/static/js/2.44c24b50.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.44c24b50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c891bac0072d36b544e2",
+    "url": "/static/js/main.0be5b366.chunk.js"
+  },
+  {
+    "revision": "de849b621e9b36141536",
+    "url": "/static/js/runtime-main.292b53d7.js"
+  }
+]);
